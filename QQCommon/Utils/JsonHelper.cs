25473471f3060a22b79804533f4f1a414463d516
userworlder <lqb9421@outlook.com>
2026-02-11 15:37:51 +0800

    完成工具类的定义

---
+++ QQCommon/Utils/JsonHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace QQCommon.Utils
+{
+    public static class JsonHelper
+    {
+        public static string Serialize(object obj)
+        {
+            return JsonConvert.SerializeObject(obj);
+        }
+
+        public static T Deserialize<T>(string json)
+        {
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+    }
+}
