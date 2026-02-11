dcac3ff7cb856f7a5d8fe85020d0a3ee7c0901e7
userworlder <lqb9421@outlook.com>
2026-02-11 17:10:19 +0800

    networkclient.cs初步实现

---
+++ QQClient.Communication/NetworkClient.cs
@@ -0,0 +1,51 @@
+﻿using QQCommon.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QQClient.Communication
+{
+    public class NetworkClient : INetworkClient
+    {
+        public event EventHandler<MessageReceivedEventArgs> MessageReceived;
+        public event EventHandler<ConnectionEventArgs> ConnectionChanged;
+
+        public bool AddFriend(string friendUsername)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Connect(string serverIp, int port)
+        {
+            return false;
+        }
+
+        public void Disconnect()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Login(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Register(string username, string password, string nickname)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SendMessage(string receiver, string content)
+        {
+            throw new NotImplementedException();
+        }
+
+        void Disconnected()
+        {
+
+        }
+
+    }
+}
