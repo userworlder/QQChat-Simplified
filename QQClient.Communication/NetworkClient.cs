cf63282fd4dbbd8e2700db8a28f875fc6e8056e5
fsrmb <1960535612@qq.com>
2026-03-13 10:28:38 +0800

    check

--- QQClient.Communication/NetworkClient.cs
+++ QQClient.Communication/NetworkClient.cs
@@ -299,7 +299,7 @@ namespace QQClient.Communication
         }
 
         public bool SendMessage(string username, string receiver, string content)
-        {
+        {//
             try
             {
                 var packet = new ChatPacket
