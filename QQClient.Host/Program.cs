2951320ba057a0bda705d753afc8afbfb44ce4e1
fsrmb <1960535612@qq.com>
2026-03-15 20:52:49 +0800

    新增调试模式

--- QQClient.Host/Program.cs
+++ QQClient.Host/Program.cs
@@ -23,21 +23,34 @@ namespace QQClient.Host
             Application.SetCompatibleTextRenderingDefault(false);
             string ip = "127.0.0.1";
             int port = 8888;
-            // 创建唯一的 NetworkClient 实例
-            //var client = new NetworkClient();
-            //bool con = client.Connect(ip, port);
-            //if (con)
-            //{
-            //    //
-            //    GlobalClient.Current = client;
-            //    Application.Run(new QQClient.UI.login());
-            //}
-            //else
-            //{
-            //    MessageBox.Show("连接服务器失败");
-            //}
-            Application.Run(new QQClient.UI.chat_new("1", "1"));
-            //Application.Run(new QQClient.UI.user());
+            DialogResult result = MessageBox.Show(
+               "是否进入调试模式？\n\n是：跳过连接，直接打开测试窗口\n否：正常连接服务器并登录",
+               "启动选项",
+               MessageBoxButtons.YesNo,
+               MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                Application.Run(new QQClient.UI.chat_new("1", "1"));
+                //Application.Run(new QQClient.UI.user());
+            }
+            else
+            {
+                //创建唯一的 NetworkClient 实例
+                var client = new NetworkClient();
+                bool con = client.Connect(ip, port);
+                if (con)
+                {
+                    //
+                    GlobalClient.Current = client;
+                    Application.Run(new QQClient.UI.login());
+                }
+                else
+                {
+                    MessageBox.Show("连接服务器失败");
+                }
+            }
+
+
         }
     }
 }
