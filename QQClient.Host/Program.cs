73c7e922b79e2824d6849cbaa81b9bf81ab66029
fsrmb <1960535612@qq.com>
2026-03-22 00:16:20 +0800

    修改Load

--- QQClient.Host/Program.cs
+++ QQClient.Host/Program.cs
@@ -21,8 +21,8 @@ namespace QQClient.Host
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            string ip = "127.0.0.1";
-            //string ip = "192.168.24.61";
+            //string ip = "127.0.0.1";
+            string ip = "192.168.24.61";
             //string ip = "192.168.24.81";
             int port = 8888;
 
